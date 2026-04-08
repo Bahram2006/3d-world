0113a19ebb5f70d43e036aed1f6a6979384b77f0
Bahram <yardam034@gmail.com>
2026-04-08 15:37:40 +0500

    add planet click interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 
-function Planet({ position, color }) {
+function Planet({ position, color, name }) {
   return (
-    <mesh position={position}>
+    <mesh
+      position={position}
+      onClick={() => {
+        console.log(name + " clicked");
+      }}
+    >
       <sphereGeometry args={[1, 32, 32]} />
       <meshStandardMaterial color={color} />
     </mesh>
@@ -14,19 +19,15 @@ export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh", background: "black" }}>
       <Canvas>
-        {/* Stars */}
         <Stars radius={100} depth={50} count={5000} factor={4} />
 
-        {/* Light */}
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
 
-        {/* Planets */}
-        <Planet position={[-3, 0, 0]} color="blue" />
-        <Planet position={[3, 0, 0]} color="purple" />
-        <Planet position={[0, 2, -2]} color="green" />
+        <Planet position={[-3, 0, 0]} color="blue" name="About" />
+        <Planet position={[3, 0, 0]} color="purple" name="Projects" />
+        <Planet position={[0, 2, -2]} color="green" name="Contact" />
 
-        {/* Controls */}
         <OrbitControls />
       </Canvas>
     </div>
